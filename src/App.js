8f03aef9adbf5ceefb3e0b395ed9fec3cd11fe0c
Gleb Rosev <rossevgleb@gmail.com>
2020-08-18 21:38:39 +0300

    Added redux

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+import Card from "./components/Calculator";
+import Output from "./components/Output";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <Output />
+    </div>
+  );
+}
+
+export default App;
